e13163f217e42c3425b2351c4dee130e73887a2b
husong <husong@sgcctd.com>
2023-07-26 16:44:43 +0800

    test1

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>HelloWorld !!!</div>
+  <div>
+    <button @click="num++">t-1 : {{ num }}</button>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const num = ref(0);
+</script>
+<style scoped></style>
