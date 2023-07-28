8f98a02e87efdcdebc770991d1dbf14786da08ca
xfei314 <34273688+xfei314@users.noreply.github.com>
2023-07-28 03:05:38 +0000

    Deploying to gh-pages from @ xfei314/xfei314.github.io@e0b1934c9f525e5b6e6bd8f34af610bd2b9939d7 🚀

---
+++ assets/NoPermission-b09882de.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-1e7b5c9f.js";await Promise.all([window.__OSL.import("vue")]);const{openBlock:o,createElementBlock:_,pushScopeId:r,popScopeId:d}=window.__OSL.getModule("vue"),c={},s={class:"no-permission"};function n(t,p){return o(),_("div",s,"无权限访问")}const a=e(c,[["render",n],["__scopeId","data-v-940bf35c"]]);export{a as default};
--- assets/package-db369734.js
+++
@@ -1,1 +0,0 @@
-const a="xdp-portal";export{a as n};
